92af2989cecfe50b3cb5f806230e36df54ffa327
Ben Pate <ben@pate.org>
2024-12-02 16:46:53 -0700

    Add more known paths

--- dome/constant_paths.go
+++ dome/constant_paths.go
@@ -1,6 +1,11 @@
 package dome
 
+// KnownPaths lists out common paths that are scanned by bots
+// for vunerabilities.
 var KnownPaths = []string{
-	"/wp-admin",
-	"/wp-content",
+	"/wp-admin",    // WordPress admin pages
+	"/wp-content",  // WordPress content directory
+	"/wp-includes", // WordPress includes directory
+	"/.cgi-bin",    // CGI directory
+	".php",         // WordPress XML-RPC interface
 }
